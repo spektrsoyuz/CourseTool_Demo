f8a6a070d974bba3b0d8f501644cfaf8a3efdada
SpektrSoyuz <schristie1109@yahoo.com>
2024-02-05 08:33:21 -0500

    add getSections(), try/except for file not found error

--- course_funcs.py
+++ course_funcs.py
@@ -1,5 +1,6 @@
 """
-CourseTool
+course_funcs.py
+Created by Seth Christie on 2/4/2024
 """
 
 import pandas as pd
@@ -11,15 +12,37 @@ import warnings
 warnings.filterwarnings("ignore", category=MarkupResemblesLocatorWarning)
 
 
+# -------------------------------------------------- functions ---------------------------------------------------------
+
 def strip_html(html_text):
+    """
+    Function to strip HTML code from a String
+    :param html_text: HTML code to be stripped
+    :return: String with HTML code stripped
+    """
     soup = BeautifulSoup(html_text, 'html.parser')
     plain_text = soup.get_text()
     return plain_text
 
 
-def getCourseData(term, catalog, catalog_url, exportJSON):
+def getCourseData(term, catalog, catalog_url, exportJSON, includeAll):
+    """
+    Function to parse through Kettering Courses A-Z and the Kettering
+    Argos Class Schedule to create a dictionary containing available courses
+    for a given term.
+    :param term: Specified school term      'winter24', 'spring23', etc.
+    :param catalog: List of acceptable course tags
+    :param catalog_url: URL to the course catalog (undergrad/grad)
+    :param exportJSON: Should the function export a json file containing the courses?
+    :param includeAll: Should the function include courses with no sections?
+    :return: Dictionary containing list of available courses
+    """
     courseList = {}  # Dictionary to store course information
-    df = pd.read_csv(term + '.csv')  # Read course data from a CSV file
+    try:
+        df = pd.read_csv(f'{term}.csv')  # Read course data from a CSV file
+    except FileNotFoundError as e:
+        print(e)
+        exit(1)
 
     # Clean up dataframe
     df = df.drop(columns=['TYPE', 'PART', 'MAX', 'WL_Max', 'WL_Actual', 'CAMPUS'])
@@ -36,88 +59,59 @@ def getCourseData(term, catalog, catalog_url, exportJSON):
 
             # Extract course information from the parsed HTML
             courseblocks = parsedData.find_all('div', 'courseblock')
+
+            # Iterate over each courseblock and add to dictionary
             for courseblock in courseblocks:
-                course = {}
                 courseblocktitle = courseblock.find('p', 'courseblocktitle').text.split('\xa0')
 
                 subjects = df['SUBJ'].values
                 tags = df['NUMB'].values
                 courseids = [f'{subject}-{tag}' for subject, tag in zip(subjects, tags)]
 
-                if courseblocktitle[0] in courseids:
-                    courseblockdesc = str(courseblock.find('p', 'courseblockdesc')).split('<br/>')
+                if courseblocktitle[0] not in courseids and not includeAll:
+                    continue
 
-                    coreqs = 'None'
-                    prereqs = 'None'
-                    standing = 'Freshman'
-                    sections = {}
+                courseblockdesc = str(courseblock.find('p', 'courseblockdesc')).split('<br/>')
 
-                    desc = strip_html(courseblockdesc[-3]).replace('\n', ' ')
+                coreqs = 'None'
+                prereqs = 'None'
+                standing = 'Freshman'
 
-                    for line in courseblockdesc:
-                        # check for class standing
-                        if 'Minimum Class Standing:' in line:
-                            standing = line.split(':')[1].strip()
+                desc = strip_html(courseblockdesc[-3]).replace('\n', ' ')
 
-                        # check for prereqs
-                        if 'Prerequisites:' in line:
-                            prereqs = strip_html(line).replace('Prerequisites: ', '')
+                for line in courseblockdesc:
+                    # Check for class standing
+                    if 'Minimum Class Standing:' in line:
+                        standing = line.split(':')[1].strip()
 
-                        # check for coreqs
-                        if 'Corequisites:' in line:
-                            coreqs = strip_html(line).replace('Corequisites: ', '')
+                    # Check for prereqs
+                    if 'Prerequisites:' in line:
+                        prereqs = strip_html(line).replace('Prerequisites: ', '')
 
-                    # check if special topics
-                    if '391' in courseblocktitle[0]:
-                        desc = 'None'
+                    # Check for coreqs
+                    if 'Corequisites:' in line:
+                        coreqs = strip_html(line).replace('Corequisites: ', '')
 
-                    title_parts = courseblocktitle[0].split('-')
+                # Check if special topics
+                if '391' in courseblocktitle[0]:
+                    desc = 'None'
 
-                    # handle section data
-                    df_sections = df[(df['SUBJ'] == title_parts[0]) & (df['NUMB'] == title_parts[1])].copy()
-                    df_sections = df_sections.drop(columns=['SUBJ', 'NUMB', 'CRN', 'CH', 'TITLE'])
-                    dict_sections = df_sections.to_dict(orient='records')
+                # Format course
+                course = {
+                    'tag': courseblocktitle[0],
+                    'name': courseblocktitle[2].replace('\n', ''),
+                    'coreqs': coreqs.replace('\n', ''),
+                    'prereqs': prereqs.replace('\n', ''),
+                    'standing': standing,
+                    'desc': desc.replace('  ', ' '),
+                    'sections': getSections(df, courseblocktitle),
+                    'credits': courseblocktitle[-1].replace(' Credits', '')
+                }
 
-                    for entry in dict_sections:
-                        # parse course dates
-                        dates = [entry['M'], entry['T'], entry['W'], entry['TH'], entry['F']]
-                        dates = [item for item in dates if item != ' ']
+                # Dump courseblock into courses
+                courses[courseblocktitle[0]] = course
 
-                        # get section data
-                        section = entry['SEC']
-                        instructor = entry['INSTRUCTOR']
-                        time = entry['TIME']
-                        date = ', '.join(dates)
-                        building = entry['BLDG']
-                        room = entry['ROOM']
-                        avail = entry['AVAIL']
-
-                        # format section block
-                        sectionblock = {
-                            'instructor': instructor,
-                            'time': time,
-                            'date': date,
-                            'building': building,
-                            'room': room,
-                            'avail': avail
-                        }
-
-                        # dump block into sections
-                        sections[section] = sectionblock
-
-                    # dump course data into course
-                    course['tag'] = courseblocktitle[0]
-                    course['name'] = courseblocktitle[2].replace('\n', '')
-                    course['coreqs'] = coreqs.replace('\n', '')
-                    course['prereqs'] = prereqs.replace('\n', '')
-                    course['standing'] = standing
-                    course['desc'] = desc.replace('  ', ' ')
-                    course['sections'] = sections
-                    course['credits'] = courseblocktitle[-1].replace(' Credits', '')
-
-                    # dump courseblock into courses
-                    courses[courseblocktitle[0]] = course
-
+            # Add course dictionary to final course list
             courseList[cat] = courses
 
     # Save the parsed course information in a JSON file
@@ -125,6 +119,48 @@ def getCourseData(term, catalog, catalog_url, exportJSON):
         filename = term + '.json'
         with open(filename, 'w', encoding='utf-8') as file:
             json.dump(courseList, file, ensure_ascii=False, indent=2)
-        print('\nSaved courses to', filename)
+        print(f'\nSaved courses to {filename}.')
 
     return courseList
+
+
+def getSections(df, course):
+    """
+    Function to return a dictionary containing each section for a given course
+    :param df: Dataframe containing sections
+    :param course: Course tag       'MATH-204', 'ECON-201', etc.
+    :return:
+    """
+    sections = {}
+    title_parts = course[0].split('-')
+    df_sections = df[(df['SUBJ'] == title_parts[0]) & (df['NUMB'] == title_parts[1])].copy()
+    dict_sections = df_sections.to_dict(orient='records')
+
+    for entry in dict_sections:
+        # Parse course dates
+        dates = [entry['M'], entry['T'], entry['W'], entry['TH'], entry['F']]
+        dates = [item for item in dates if item != ' ']
+
+        # Get section data
+        section = entry['SEC']
+        instructor = entry['INSTRUCTOR']
+        time = entry['TIME']
+        date = ', '.join(dates)
+        building = entry['BLDG']
+        room = entry['ROOM']
+        avail = entry['AVAIL']
+
+        # Format section block
+        sectionblock = {
+            'instructor': instructor,
+            'time': time,
+            'date': date,
+            'building': building,
+            'room': room,
+            'avail': avail
+        }
+
+        # Dump block into sections
+        sections[section] = sectionblock
+
+    return sections
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 """
-CourseTool
+main.py
+Created by Seth Christie on 2/4/2024
 """
-
 import course_funcs
 
 # constants
-catalog_url = 'https://catalog.kettering.edu/coursesaz/undergrad/'
-catalog = [
-    'BIOL', 'BUSN', 'CHME', 'CHEM', 'COMM', 'CE', 'CS', 'ECON', 'ECE', 'EE', 'ENGR', 'EP', 'HIST',
-    'HUMN', 'IME', 'CILE', 'LANG', 'LA', 'LIT', 'MGMT', 'MATH', 'MECH', 'MEDI', 'PHIL', 'PHYS', 'PSYC',
-    'SSCI', 'SOC'
+undergrad = 'https://catalog.kettering.edu/coursesaz/undergrad/'
+undergrad_catalog = [
+    'BIOL', 'BUSN', 'CHME', 'CHEM', 'COMM', 'CE', 'CS', 'ECON', 'ECE', 'EE', 'ENGR', 'EP', 'HIST', 'HUMN', 'IME',
+    'CILE', 'LANG', 'LA', 'LIT', 'MGMT', 'MATH', 'MECH', 'MEDI', 'PHIL', 'PHYS', 'PSYC', 'SSCI', 'SOC'
 ]
 
-# ------------------------------------------------------- main ---------------------------------------------------------
+grad = 'https://catalog.kettering.edu/coursesaz/grad/'
+grad_catalog = [
+    'ACCT', 'BUSN', 'COMM', 'CE', 'CS', 'ECE', 'EE', 'ENGR', 'FINC', 'IME', 'ISYS', 'MFGO', 'MGMT', 'MKRT', 'MATH',
+    'MECH'
+]
+
+# ----------------------------------------------------- main -----------------------------------------------------------
 
 if __name__ == '__main__':
     # Run the script to fetch and parse course data
-    course_funcs.getCourseData('summer24', catalog, catalog_url, True)
+    courseData = course_funcs.getCourseData('summer24_undergrad', undergrad_catalog, undergrad, True, True)
+    courseData2 = course_funcs.getCourseData('summer24_grad', grad_catalog, grad, True, True)
