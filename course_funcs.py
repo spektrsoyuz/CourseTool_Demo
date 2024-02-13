099f648c821d1ebad1f5836addbf11a5280bd09c
SpektrSoyuz <schristie1109@yahoo.com>
2024-02-13 02:38:11 -0500

    added dictToDf(), support for xlsx/yaml exports

--- course_funcs.py
+++ course_funcs.py
@@ -8,7 +8,9 @@ import warnings
 
 import pandas as pd
 import requests
+import yaml
 from bs4 import BeautifulSoup, MarkupResemblesLocatorWarning
+from styleframe import StyleFrame
 
 warnings.filterwarnings("ignore", category=MarkupResemblesLocatorWarning)
 
@@ -17,6 +19,9 @@ cfilter = ['COMM', 'ECON', 'BUSN', 'MGMT', 'HIST', 'HUMN', 'CILE', 'LA', 'LIT', 
            'MECH-330', 'MECH-331', 'MECH-420', 'MECH-422', 'MECH-430', 'MECH-431']
 
 cafilter = ['BUSN-303', 'BUSN-304', 'MGMT-310', 'MGMT-419', 'MGMT-546', 'MECH-448', 'MECH-495']
+
+heads = ['Tag', 'Name', 'Coreqs', 'Prereqs', 'Standing', 'Instructor', 'Time', 'Date',
+         'Building', 'Room', 'Avail']
 
 
 # -------------------------------------------------- functions ---------------------------------------------------------
@@ -41,7 +46,7 @@ def getCourseData(csvFile, catalog, catalogURL, includeAll):
     :param catalog: List of acceptable course tags
     :param catalogURL: URL to the course catalog (undergrad/grad)
     :param includeAll: Should the function include courses with no sections?
-    :return: Dictionary containing list of available courses
+    :return: Dictionary containing a list of available courses
     """
     courseList = {}  # Dictionary to store course information
     try:
@@ -49,6 +54,8 @@ def getCourseData(csvFile, catalog, catalogURL, includeAll):
     except FileNotFoundError as e:
         print(e)
         exit(1)
+
+    print(f'Retrieving Course Data from {csvFile}')
 
     # Clean up dataframe
     df = df.drop(columns=['TYPE', 'PART', 'MAX', 'WL_Max', 'WL_Actual', 'CAMPUS'])
@@ -127,7 +134,7 @@ def getSections(df, course):
     """
     Function to return a dictionary containing each section for a given course
     :param df: Dataframe containing sections
-    :param course: Course tag       'MATH-204', 'ECON-201', etc.
+    :param course: Course tag 'MATH-204', 'ECON-201', etc.
     :return: Dictionary containing sections for a given course
     """
     sections = {}
@@ -167,7 +174,7 @@ def getSections(df, course):
 
 def getMEElectives(filename):
     """
-    Function to return a dictionary containing all course eligible as ME Electives
+    Function to return a dictionary containing all courses eligible as ME Electives
     :param filename: Name of the json file containing all courses
     :return: Dictionary containing all ME Elective options
     """
@@ -185,7 +192,7 @@ def getMEElectives(filename):
                 num = num[:-1]
 
             # Check if course is > 300 and < 600
-            if 300 > int(num) > 600:
+            if not (300 <= int(num) <= 600):
                 continue
 
             # Check course against filter
@@ -202,6 +209,33 @@ def getMEElectives(filename):
     return electives
 
 
+def dictToDf(courses):
+    """
+    Function to convert a Dictionary of courses into a Pandas DataFrame
+    :param courses: Dictionary containing courses
+    :return: DataFrame containing courses
+    """
+    data = []
+    for subject in courses:
+        for courseblock in courses[subject]:
+            for section in courses[subject][courseblock]['sections']:
+                data.append([
+                    courses[subject][courseblock]['tag'],  # tag
+                    courses[subject][courseblock]['name'],  # name
+                    courses[subject][courseblock]['coreqs'],  # coreqs
+                    courses[subject][courseblock]['prereqs'],  # prereqs
+                    courses[subject][courseblock]['standing'],  # standing
+                    courses[subject][courseblock]['sections'][section]['instructor'],  # instructor
+                    courses[subject][courseblock]['sections'][section]['time'],  # time
+                    courses[subject][courseblock]['sections'][section]['date'],  # date
+                    courses[subject][courseblock]['sections'][section]['building'],  # building
+                    courses[subject][courseblock]['sections'][section]['room'],  # room
+                    courses[subject][courseblock]['sections'][section]['avail'],  # avail
+                ])
+
+    return StyleFrame(pd.DataFrame(data, columns=heads))
+
+
 def exportCourses(courses, filetype, filename):
     """
     Function to export a dictionary of courses to a given file format
@@ -214,6 +248,19 @@ def exportCourses(courses, filetype, filename):
         case 'json':
             with open(filename, 'w', encoding='utf-8') as file:
                 json.dump(courses, file, ensure_ascii=False, indent=2)
+
+        case 'yaml':
+            with open(filename, 'w', encoding='utf-8') as file:
+                yaml.dump(courses, file)
+
+        case 'xlsx':
+            with StyleFrame.ExcelWriter(filename) as writer:
+                sf = dictToDf(courses)
+                sf.to_excel(
+                    excel_writer=writer,
+                    best_fit=heads
+                )
+
         case _:
             with open(filename, 'w', encoding='utf-8') as file:
                 json.dump(courses, file, ensure_ascii=False, indent=2)
--- main.py
+++ main.py
@@ -2,28 +2,31 @@
 main.py
 Created by Seth Christie on 2/4/2024
 """
-import course_funcs
-
-# constants
-undergrad = 'https://catalog.kettering.edu/coursesaz/undergrad/'
-undergrad_catalog = [
-    'BIOL', 'BUSN', 'CHME', 'CHEM', 'COMM', 'CE', 'CS', 'ECON', 'ECE', 'EE', 'ENGR', 'EP', 'HIST', 'HUMN', 'IME',
-    'CILE', 'LANG', 'LA', 'LIT', 'MGMT', 'MATH', 'MECH', 'MEDI', 'PHIL', 'PHYS', 'PSYC', 'SSCI', 'SOC'
-]
-
-grad = 'https://catalog.kettering.edu/coursesaz/grad/'
-grad_catalog = [
-    'ACCT', 'BUSN', 'COMM', 'CE', 'CS', 'ECE', 'EE', 'ENGR', 'FINC', 'IME', 'ISYS', 'MFGO', 'MGMT', 'MKRT', 'MATH',
-    'MECH'
-]
+import yaml
+import course_funcs as cf
 
 # ----------------------------------------------------- main -----------------------------------------------------------
 
 if __name__ == '__main__':
+    # Read config.yml
+    with open('config.yml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    undergrad_catalog = config['catalog']['undergrad']['tags']
+    grad_catalog = config['catalog']['undergrad']['tags']
+    undergrad_url = config['catalog']['undergrad']['url']
+    grad_url = config['catalog']['grad']['url']
+
     # Run the script to fetch and parse course data
-    courseData = course_funcs.getCourseData('example_s24.csv', undergrad_catalog, undergrad, False)
-    course_funcs.exportCourses(courseData, 'json', 'Exports/s24_undergrad.json')
+    undergradData = cf.getCourseData('summer24_all.csv', undergrad_catalog, undergrad_url, False)
+    gradData = cf.getCourseData('summer24_grad.csv', grad_catalog, grad_url, False)
+    cf.exportCourses(undergradData, 'json', 'Exports/s24_undergrad.json')
+    cf.exportCourses(undergradData, 'xlsx', 'Exports/s24_undergrad.xlsx')
+    cf.exportCourses(gradData, 'json', 'Exports/s24_grad.json')
+    cf.exportCourses(gradData, 'xlsx', 'Exports/s24_grad.xlsx')
 
     # Get ME Electives from term
-    me_electives = course_funcs.getMEElectives('Exports/s24_undergrad.json')
-    course_funcs.exportCourses(me_electives, 'json', 'Exports/s24_mechelv.json')
+    me_electives = cf.getMEElectives('Exports/s24_undergrad.json')
+    cf.exportCourses(me_electives, 'json', 'Exports/s24_mechelv.json')
+    cf.exportCourses(me_electives, 'xlsx', 'Exports/s24_mechelv.xlsx')
+    cf.exportCourses(me_electives, 'yaml', 'Exports/s24_mechelv.yml')
